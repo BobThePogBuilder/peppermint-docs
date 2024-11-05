7ed0590fec36e07a0c66ce39ef8139e21fac49bc
BobThePogBuilder <102852787+BobThePogBuilder@users.noreply.github.com>
2024-11-04 19:26:49 -0800

    new test

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,6 @@
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
 
-import cloudflare from '@astrojs/cloudflare';
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [starlight({
@@ -34,6 +32,4 @@ export default defineConfig({
       ],
   }),],
 
-  output: 'server',
-  adapter: cloudflare(),
 });
